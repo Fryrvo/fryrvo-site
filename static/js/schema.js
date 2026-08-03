70bbd166b9bbf7e3bf06f708f9ae633548f7604c
Fryrvo Fox <Fryrvo@fryrvo.com>
2026-08-03 08:01:33 +0800

    Create schema.js

---
+++ static/js/schema.js
@@ -0,0 +1,16 @@
+<script type="application/ld+json">
+{
+  "@context": "https://schema.org",
+  "@type": "Person",
+  "name": "Fryrvo Fox",
+  "alternateName": "Fryrvo",
+  "jobTitle": ["Musician", "Developer"],
+  "sameAs": [
+    "https://github.com/Fryrvo",
+    "https://x.com/Fryrvo",
+    "https://soundcloud.com/fryrvo",
+    "https://open.spotify.com/artist/6D4MMKyp97IcBtp3U43Xb3",
+    "https://music.apple.com/us/artist/fryrvo/678042795"
+  ]
+}
+</script>
